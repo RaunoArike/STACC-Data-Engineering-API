4d114f6c1d39ea400029574b162125a39e4f20f4
Rauno Arike <63519956+RaunoArike@users.noreply.github.com>
2022-03-18 18:38:03 +0100

    Add setup information

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setuptools.setup(
+    name="STACC-Data-Engineering-API",
+    author="Rauno Arike",
+    description="A web API for the STACC data engineering test task",
+    long_description=LONG_DESCRIPTION"
+    url="https://github.com/RaunoArike/STACC-Data-Engineering-API",
+    packages=setuptools.find_packages(exclude=("tests")),
+    python_requires=">=3.7"
+)
