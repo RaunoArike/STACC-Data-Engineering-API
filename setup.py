c99942eabf7bdd101552a418b8fdf3533d8307cf
Rauno Arike <63519956+RaunoArike@users.noreply.github.com>
2022-03-18 18:41:03 +0100

    Fix minor errors

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="STACC-Data-Engineering-API",
     author="Rauno Arike",
     description="A web API for the STACC data engineering test task",
-    long_description=LONG_DESCRIPTION"
+    long_description=LONG_DESCRIPTION,
     url="https://github.com/RaunoArike/STACC-Data-Engineering-API",
     packages=setuptools.find_packages(exclude=("tests")),
     python_requires=">=3.7"
